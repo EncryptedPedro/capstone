a5050097ed0fab2b1755dc5a55eca23bd589241d
EncryptedPedro <pedroakhaiz@gmail.com>
2024-12-23 20:20:19 +0000

    components created

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Meta Front End Capstone</h1>
+            <img src="/icons_assets/Logo.svg" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <main>
+            <h2>Welcome to the Meta Front End Capstone!</h2>
+            <p>This is the main content area. You can add more components here.</p>
+        </main>
+    );
+}
+
+export default Main;
