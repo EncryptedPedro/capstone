2ea28a1d9f05e36c4d944a0cffee5964172269f0
EncryptedPedro <pedroakhaiz@gmail.com>
2025-01-06 23:01:15 +0000

    improved css styling, fixed image positioning

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReserveButton from "./ReserveButton";
+import RestauranFood from "../icons_assets/restauranfood.jpg";
 
 const Main = () => {
     return (
@@ -8,8 +9,11 @@ const Main = () => {
                 <h2>Little Lemon</h2>
                 <p>Chicago</p>
                 <p>Little Lemon is a family-owned restaurant that has been serving the Chicago community for over 20 years. Our mission is to provide our customers with the best dining experience</p>
+                < ReserveButton />
             </div>
-            < ReserveButton />
+            <div>
+                <img src={RestauranFood} alt="Image" width="300px"></img>
+            </div>
         </main>
     );
 }
